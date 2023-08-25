a984bf3b49dd47cd48d68c007af3a2f89595d9ba
davidkhachatryan02 <dkhachatryan043@gmail.com>
2023-08-25 19:14:45 +0400

    commit

--- src/app/home/components/UserAccounts/index.jsx
+++ src/app/home/components/UserAccounts/index.jsx
@@ -1,9 +1,9 @@
 "use client";
 // import { useSelector } from "react-redux";
-import api from "~/api";
+//import api from "~/api";
 import { TextField, InputAdornment } from "@mui/material";
-import Cookies from "js-cookie";
-import axios from "axios";
+//import Cookies from "js-cookie";
+//import axios from "axios";
 //import AndroidIcon from "@mui/icons-material/Android";
 //import GitHubIcon from "@mui/icons-material/GitHub";
 // import { selectUser } from "~/store/selectors/userSelectors";
@@ -14,27 +14,27 @@ const styles = {
 };
 
 const UserAccounts = () => {
-  Cookies.set(
-    "accessToken",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjkyOTY2OTc0LCJleHAiOjE2OTI5NjcwNzR9.0luyMHbtZJxPn4N5dBEgYeteNxxsKLUY9HWPRXUVe7c"
-  );
-  Cookies.set("refreshToken", "testUser1refreshToken");
+  // Cookies.set(
+  //   "accessToken",
+  //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjkyOTY2OTc0LCJleHAiOjE2OTI5NjcwNzR9.0luyMHbtZJxPn4N5dBEgYeteNxxsKLUY9HWPRXUVe7c"
+  // );
+  // Cookies.set("refreshToken", "testUser1refreshToken");
 
-  const getUserData = async () => {
-    try {
-      const response = await api.auth.getUser();
-      const { slackUserName, gitHubUserName } = response;
+  // const getUserData = async () => {
+  //   try {
+  //     const response = await api.auth.getUser();
+  //     const { slackUserName, gitHubUserName } = response;
 
-      console.log("Slack User Name:", slackUserName);
-      console.log("GitHub User Name:", gitHubUserName);
+  //     console.log("Slack User Name:", slackUserName);
+  //     console.log("GitHub User Name:", gitHubUserName);
 
-      return { slackUserName, gitHubUserName };
-    } catch (error) {
-      console.error("Error fetching user data:", error);
-      throw error;
-    }
-  };
-  getUserData().then().catch();
+  //     return { slackUserName, gitHubUserName };
+  //   } catch (error) {
+  //     console.error("Error fetching user data:", error);
+  //     throw error;
+  //   }
+  // };
+  // getUserData().then().catch();
 
   return (
     <div className={styles.container}>
